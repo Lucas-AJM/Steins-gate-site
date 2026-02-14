d99cf6c01a6c323aea9364668e21a413db49145f
Lucas <Lucask10fhi@gmail.com>
2026-02-14 00:48:56 -0300

    Replace entrar and surpresa functions with abrir

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
-function entrar() {
-    document.getElementById("tela1").classList.add("hidden");
-    document.getElementById("tela2").classList.remove("hidden");
-}
-
-function surpresa() {
-    document.getElementById("mensagemFinal").classList.remove("hidden");
-    criarCoracoes();
-}
-
-function criarCoracoes() {
-    setInterval(() => {
-        let coracao = document.createElement("div");
-        coracao.innerHTML = "❤️";
-        coracao.style.position = "absolute";
-        coracao.style.left = Math.random() * 100 + "vw";
-        coracao.style.top = "100vh";
-        coracao.style.fontSize = "24px";
-        coracao.style.animation = "subir 4s linear forwards";
-        document.body.appendChild(coracao);
-
-        setTimeout(() => {
-            coracao.remove();
-        }, 4000);
-    }, 300);
+function abrir() {
+  document.querySelector(".card").classList.add("hidden");
+  document.getElementById("conteudo").classList.remove("hidden");
 }
